41a9ca9986ee0cbba5e2627aac951258d5f60514
news.dev.jorge <news.dev.jorge@gmail.com>
2026-02-21 19:11:25 +0000

    fix: Corrige componentes da home para usar API

--- src/components/Home/CategoriasProdutos.js
+++ src/components/Home/CategoriasProdutos.js
@@ -1,19 +1,44 @@
-import React from 'react'
-import classes from './CategoriasProdutos.module.css'
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useApi } from '../../hooks/useApi';
+import classes from './CategoriasProdutos.module.css';
+import Loading from '../Utility/Loading';
 
 const CategoriasProdutos = () => {
+  // Busca apenas as categorias que estão marcadas como ativas
+  const { data: categorias, loading, error } = useApi('categorias', { ativo: 'eq.true', _sort: 'ordem' });
+
+  const renderCategorias = () => {
+    if (loading) {
+      return <Loading />;
+    }
+
+    if (error) {
+      return <li className={classes.error}>Erro ao carregar categorias.</li>;
+    }
+
+    if (categorias && categorias.length > 0) {
+      return categorias.map(categoria => (
+        <li key={categoria.id}>
+          {/* O link levará para a página de produtos, futuramente podemos fazer um scroll até a categoria */}
+          <Link to="/produtos">{categoria.nome}</Link>
+        </li>
+      ));
+    }
+
+    return <li>Nenhuma categoria encontrada.</li>;
+  };
+
   return (
     <section className={classes.container}>
-      <div className='container'>
+      <div className="container">
         <ul className={classes.listaCategorias}>
-          <li>Açaí</li>
-          <li>Picolé</li>
-          <li>Sorvete</li>
-          <li>Especiais</li>
+          {renderCategorias()}
         </ul>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default CategoriasProdutos
+export default CategoriasProdutos;
--- src/components/Home/MaisVendidos.js
+++ src/components/Home/MaisVendidos.js
@@ -1,25 +1,48 @@
-import React, { lazy, Suspense } from 'react'
+
+import React, { lazy, Suspense } from 'react';
 import { Link } from 'react-router-dom';
-import produtos from '../../produtos/produtos';
+import { useApi } from '../../hooks/useApi';
 import classes from './MaisVendidos.module.css';
-import Loading from '../Utility/Loading'
+import Loading from '../Utility/Loading';
+
 const Slider = lazy(() => import('../Utility/Slider'));
 
 const MaisVendidos = () => {
+  // Busca produtos que são 'destaque' e 'ativos' do banco de dados
+  const { data: maisVendidos, loading, error } = useApi('produtos', {
+    destaque: 'eq.true', 
+    ativo: 'eq.true',
+    _limit: 10 // Limita a 10 produtos para não sobrecarregar a home
+  });
 
-  
+  const renderContent = () => {
+    if (loading) {
+      return <Loading />;
+    }
+
+    if (error) {
+      return <p className={classes.error}>Não foi possível carregar os mais vendidos.</p>;
+    }
+
+    if (maisVendidos && maisVendidos.length > 0) {
+      return <Slider items={maisVendidos} />;
+    }
+
+    return <p>Nenhum produto em destaque no momento.</p>;
+  };
+
   return (
     <section className={classes.container}>
       <div className='container'>
         <h2>Mais vendidos</h2>
         <p>Os mais pedidos da semana!</p>
         <Suspense fallback={<Loading />}>
-        <Slider items={produtos.maisVendidos}/>
+          {renderContent()}
         </Suspense>
         <Link to="/produtos" className='btn-amarelo'>Confira todos os produtos</Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default MaisVendidos
+export default MaisVendidos;
